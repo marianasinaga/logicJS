c33bc7e4fcea95dba6d64f4688bb9efe5334dd06
Mariana Sinaga <if417028@students.del.ac.id>
2019-07-08 20:15:42 +0700

    duplicate file exercise11

---
+++ exercise11 - Copy.js
@@ -0,0 +1,15 @@
+// X dan O
+
+function xo(str){
+    let x = 'x';
+    let o = 'o';
+    
+    if(String(x).length == String(o).length){
+        'true';
+    }
+    else{
+        'false';
+    }
+}
+
+console.log(xo('xoxo'));
